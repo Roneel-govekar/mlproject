e8c928802842bbf9a694f7fa12c3c1f001931779
Roneel Govekar <roneelgovekar@yahoo.com>
2023-06-15 23:46:07 -0400

    model prediction

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,9 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass 
 from src.components.data_transformation import DataTranformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import model_trainer_config
+from src.components.model_trainer import ModelTrainer
+
 @dataclass
 class DataIngestionConfig:
     train_data_path:str=os.path.join("artifact","train.csv")
@@ -49,6 +52,7 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTranformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_array,test_array,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
-
+    model_trainer=ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_array,test_array))
